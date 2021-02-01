385ddb15864f2ab2364a55ce84d3dd3aac1b1fb3
saurav <spmtripathi10@gmail.com>
2021-02-01 18:33:32 +0530

    created different classes

--- src/com/company/Model.java
+++ src/com/company/Model.java
@@ -14,7 +14,37 @@ public class Model {
 
     public DataFrame getFrame () { return this.dataFrame; }
 
-    public void readCSV(File fiel)
+    public void readCSV(File file) throws FileNotFoundException {
+        dataFrame = new DataLoader().readCSV(file);
+    }
+
+    public void readJSON(File file) throws FileNotFoundException {
+        dataFrame = new DataLoader().readJSON(file);
+    }
+
+    public String[] getColumnNames() {return this.dataFrame.getColumnNames(); }
+
+    public String[][] getRows() throws Exception {
+        return this.dataFrame.getRows();
+
+    }
+
+    public String[][] getPatientData() throws Exception {
+
+        String[][] patients;
+        patients = new String[this.dataFrame.getRowCount()][this.dataFrame.getColumnCount()];
+        int i = 0;
+        while (i < this.dataFrame.getRowCount()) {
+            int j = 0;
+            while (j < this.dataFrame.getColumnCount() - 1) {
+                patients[i][j] = this.dataFrame.getValue(j,i);
+                j++;
+            }
+            i++;
+        }
+        return patients;
+    }
+
 
 
 }
