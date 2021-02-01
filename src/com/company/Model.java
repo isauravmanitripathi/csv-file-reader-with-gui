19a95a6e7db1ca35585d74ee6ac71d5eadf48f4b
saurav <spmtripathi10@gmail.com>
2021-02-01 14:26:51 +0530

    created different classes

--- src/com/company/Model.java
+++ src/com/company/Model.java
@@ -1,4 +1,20 @@
 package com.company;
+import jdk.jfr.Frequency;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 
 public class Model {
+
+    private DataFrame dataFrame;
+
+    public Model () {this.dataFrame  = new DataFrame(); }
+
+    public DataFrame getFrame () { return this.dataFrame; }
+
+    public void readCSV(File fiel)
+
+
 }
