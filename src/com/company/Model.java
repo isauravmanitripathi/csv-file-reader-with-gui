ff66fd331e2736e0b4ab93df4475adc51dcf7005
saurav <spmtripathi10@gmail.com>
2021-02-01 18:38:20 +0530

    created different classes

--- src/com/company/Model.java
+++ src/com/company/Model.java
@@ -45,6 +45,40 @@ public class Model {
         return patients;
     }
 
+    private String convertDateToCorrectFormat(String date) {
+        String[] splitDate = date.split("-");
+        Collection.reverse(Arrays.asList(splitDate));
+        return splitDate[0] + "/" + splitDate[1] + "/" + splitDate[2];
+    }
+
+    private boolean isDead(int index) throws Exception {return dataFrame.getColumns().get(2).getRowValue(index).equals("");}
+
+    public String oldestPerson() throws Exception {
+        ArrayList<Date> dates;
+        dates = new ArrayList<>();
+        SimpleDateFormat formatter;
+        formatter = new SimpleDateFormat("dd/mm/yyyy");
+        ArrayList<Column> columns = dateFrame.getColumns();
+        for (int j = 0; j < columns.size(); j++) {
+            Column column = columns.get(j);
+            if (!column.getName().toLowerCase().equals("birthdate")) {
+                continue;
+            }
+            int i = 0;
+            while (i < column.getSize()) {
+                String date = isDead(i) ? column.getRowValue(i) : "2020-03-25"; // ignore dead people
+                dates.add(formatter.parse(convertDateToCorrectFormat(date)));
+                i++;
+            }
+            int index = dates.indexOf(Collections.min(dates));
+            String firstName = dataFrame.getColumns().get(7).getRowValue(index); // get first name of oldest
+            String lastName = dataFrame.getColumns().get(8).getRowValue(index); // get last name of oldest
+            return firstName + " " + lastName;
+        }
+        return null;
+
+    }
+
 
 
 }
