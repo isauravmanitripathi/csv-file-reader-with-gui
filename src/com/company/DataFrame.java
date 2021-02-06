daedb29f1f0a787f38ff0c58c9ec874def781cac
saurav <spmtripathi10@gmail.com>
2021-02-06 11:19:24 +0530

    started working on dataframe

--- src/com/company/DataFrame.java
+++ src/com/company/DataFrame.java
@@ -3,4 +3,30 @@ package com.company;
 import java.util.*;
 
 public class DataFrame {
+
+    private ArrayList<Column> columns;
+
+    public DataFrame() { this.columns = new ArrayList<Column>() ;}
+
+    public ArrayList<Column> getColumns() { return column; }
+
+    public void addColumn(String columnName) { this.columns.add(new Column(columnName));}
+
+    public boolean columnExists(String coulmnName) {
+        boolean result = false;
+        ArrayList<Column> columnArrayList = this.columns;
+        for (int i = 0; i < columnArrayList.size(); i++ ) {
+            Column column = columnArrayList.get(i);
+            if (!column.getName().equals(columnName)) {
+                continue;
+            }
+            result = true;
+            break;
+        }
+        return result;
+    }
+
+
+
+
 }
